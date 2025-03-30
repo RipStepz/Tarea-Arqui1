ee8e0620b9bbe959f84768fbf41f2e39ed161919
vicente <vsilva@usm.cl>
2025-03-30 17:08:28 -0300

    binario

--- Movimiento_Tablero.py
+++ Movimiento_Tablero.py
@@ -1,2 +1,17 @@
-def Conversion_binario_decimal():
-    
+def Conversion_binario_decimal(binario):
+
+    decimal = 0
+    longitud =  len(binario)
+
+    for i in range(longitud):
+        bit = binario[i]
+        if bit not in('0' , '1'):
+            print("Binario no valido")
+            return None
+        
+        decimal += int(bit) * (2 ** (longitud - i - 1))
+    return decimal
+
+binario = "1101"
+decimal = Conversion_binario_decimal(binario)
+print(decimal)
